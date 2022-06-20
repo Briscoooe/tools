01f758df723580714a4d6571ab52d259a3890775
Briscoooe <brianbriscoe06@gmail.com>
2022-06-20 23:04:31 +0100

    init

---
+++ custom-header.js
@@ -0,0 +1,14 @@
+// https://codepen.io/pookagehayes/pen/JjpMwGq
+const template = document.createElement("template");
+class MyComponent extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+        <ul>
+            <li><a href="character-count.html">Character count</a></li>
+            <li><a href="base64.html">Base 64 encode/decode</a></li>
+        <ul>
+    `;
+  }
+}
+
+customElements.define( 'custom-header', MyComponent );
