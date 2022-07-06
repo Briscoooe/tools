cb0f69050231b7d4755079a292ab662e77dc1dc8
Briscoooe <brianbriscoe06@gmail.com>
2022-07-06 18:37:29 +0200

    working on templates

--- components/custom-header.js
+++ components/custom-header.js
@@ -7,19 +7,19 @@ class MyComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
         <ul class="nav-list">
-            <li><a href="../pages/character-count.html">Character count</a></li>
-            <li><a href="../pages/sort.html">Sort</a></li>
-            <li><a href="../pages/casing.html">Casing</a></li>
-            <li><a href="../pages/parse-params.html">Parse params</a></li>
-            <li><a href="../pages/unix-time.html">Unix time</a></li>
-            <li><a href="../pages/url-encode-decode.html">URL encode/decode</a></li>
-            <li><a href="../pages/word-count.html">Word count</a></li>
             <li><a href="../pages/base64.html">Base 64 encode/decode</a></li>
-            <li><a href="../pages/jwt.html">JWT</a></li>
+            <li><a href="../pages/casing.html">Casing</a></li>
+            <li class="active"><a href="../pages/character-count.html">Character count</a></li>
             <li><a href="../pages/json-to-csv.html">JSON to CSV</a></li>
-            <li><a href="../pages/json.html">JSON</a></li>
+            <li><a href="../pages/json.html">JSON Parse</a></li>
+            <li><a href="../pages/jwt.html">JWT</a></li>
+            <li><a href="../pages/parse-params.html">Parse params</a></li>
             <li><a href="../pages/sed.html">Search and replace</a></li>
+            <li><a href="../pages/sort.html">Sort</a></li>
+            <li><a href="../pages/url-encode-decode.html">URL encode/decode</a></li>
             <li><a href="../pages/uuid.html">UUID</a></li>
+            <li><a href="../pages/unix-time.html">Unix time</a></li>
+            <li><a href="../pages/word-count.html">Word count</a></li>
         <ul>
     `;
   }
